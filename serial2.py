dbaa008bf89f958269464afd84489ff7cf2e9f66
r33did <71927989+r33did@users.noreply.github.com>
2021-03-07 10:59:17 +0700

    Add files via upload

---
+++ serial2.py
@@ -0,0 +1,39 @@
+import serial
+import time
+import csv
+import matplotlib
+matplotlib.use("tkAgg")
+import matplotlib.pyplot as plt
+import numpy as np
+
+ser = serial.Serial('COM6')
+ser.flushInput()
+
+plot_window = 20
+y_var = np.array(np.zeros([plot_window]))
+
+plt.ion()
+fig, ax = plt.subplots()
+line, = ax.plot(y_var)
+
+while True:
+    try:
+        ser_bytes = ser.readline()
+        try:
+            decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
+            print(decoded_bytes)
+        except:
+            continue
+        with open("test_data.csv","a") as f:
+            writer = csv.writer(f,delimiter=",")
+            writer.writerow([time.time(),decoded_bytes])
+        y_var = np.append(y_var,decoded_bytes)
+        y_var = y_var[1:plot_window+1]
+        line.set_ydata(y_var)
+        ax.relim()
+        ax.autoscale_view()
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+    except KeyboardInterrupt:
+        print("Keyboard Interrupt")
+        break
