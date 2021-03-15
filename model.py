e3e8b34853bbcd8381137e5ae9683f7e76bce923
r33did <xdaichi06@gmail.com>
2021-03-15 14:05:09 +0700

    lanjut model

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import keras
 import os
 import numpy as np
+import pandas as pd
 from keras.utils import to_categorical
 from sklearn import metrics
 from sklearn.metrics import confusion_matrix
@@ -11,11 +12,12 @@ labels = []
 def extract_data_test(folder='data_test'):
     dirs = os.listdir(folder)
     counter = 0
-    for dr in dirs:
-        for file in os.listdir(folder+'/'+dr):
-            counter+=1
+    # for dr in dirs:
+    #     for file in os.listdir(folder+'/'+dr):
+    #         counter+=1
 
-def extract_feature():
+
+def extract_feature(folder='data_test'):
     dirs = os.listdir(folder)
     count = 0
-    
+    df = pd.read_csv()
